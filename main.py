9e92e6924fb8272a105d8af960fd94c6975c175c
renanzao <renannevessilva@gmail.com>
2025-03-13 15:35:03 -0300

    Primeiro esboço para extração iterativa das conversas e requisição dos dados pro gemini

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import google.generativeai as genai
+import time
 import os
 
 API_KEY = os.getenv('GOOGLE_API_KEY')
@@ -7,16 +8,21 @@ API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key = API_KEY)
 model = genai.GenerativeModel('gemini-1.5-pro')
 
+def extracao_info(conversa):
+    pergunta = f""" 
+                Extraia as seguintes informações da conversa:
+                - Nome do cliente
+                - Orçamento
+                Conversa : {conversa}
+                """
+    resposta = model.generate_content(pergunta)
+    return resposta.text
+
+
 conversas = pd.read_csv("D:/Pessoal/desafio_morada/dados/conversas_leads.csv")
-primeira_conv = conversas.iloc[0,1]
-print(conversas.iloc[0,1])
 
-pergunta = f""" 
-            Extraia as seguintes informações da conversa:
-            - Nome do cliente
-            - Orçamento
-            Conversa : {primeira_conv}
-            """
-
-resposta = model.generate_content(pergunta)
-print(resposta.text)
+for index, row in conversas.iterrows():
+    conv = conversas.iloc[index,1]
+    time.sleep(10)
+    resp = extracao_info(conv)
+    print (resp)
