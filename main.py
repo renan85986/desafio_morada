160ddf870100165c7abc5cda06a7bf4bfd38ca72
renanzao <renannevessilva@gmail.com>
2025-03-13 14:55:33 -0300

    Chave API configurada e conexão com o gemini feita, chave armazenada em VS

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import pandas as pd
 import google.generativeai as genai
 import os
 
+API_KEY = os.getenv('GOOGLE_API_KEY')
+
+genai.configure(api_key = API_KEY)
+model = genai.GenerativeModel('gemini-1.5-pro')
+
+resposta = model.generate_content("Olá google!")
+print(resposta.text)
