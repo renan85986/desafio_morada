6a3b15788b37f9ccf7d5e5a56b2b2832f5f9a15d
renanzao <renannevessilva@gmail.com>
2025-03-14 11:20:06 -0300

    corrigido problema de conversão do json para dict

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import google.generativeai as genai
 import time
+import json
+import re
 import os
 
 API_KEY = os.getenv('GOOGLE_API_KEY')
@@ -11,25 +13,39 @@ model = genai.GenerativeModel('gemini-1.5-pro')
 def extracao_info(conversa):
     pergunta = f""" 
                 Extraia as seguintes informações da conversa:
-                - Nome do cliente
+                - Nome
                 - Email
                 - Contato
                 - Orçamento
-                - Localização desejada
-                - Tipo de imóvel
-                - Preferencias adicionais
+                - Localização 
+                - Tipo_imóvel
+                - Preferencias
                 - Duvidas mencionadas
                 Conversa : {conversa}
-                Responda em formato JSON
+                Responda estritamente em formato JSON válido, sem caracteres extras como '''json ou '''' 
+                e lembre de retornar o orçamento em formato compatível com banco de dados
                 """
     resposta = model.generate_content(pergunta)
-    return resposta.text
+    resposta = re.sub(r"^```json|```$", "", resposta.text.strip()).strip()
+    
+    print(resposta)
+    dados = json.loads(resposta) #conversão para dicionário
+    print(type(dados))
+
+    return dados
 
 
 conversas = pd.read_csv("D:/Pessoal/desafio_morada/dados/conversas_leads.csv")
+dados_processados = [] # lista vazia
 
 for index, row in conversas.iterrows():
     conv = conversas.iloc[index,1]
-    time.sleep(15)
+    time.sleep(5)
+
     resp = extracao_info(conv)
-    print (resp)
+    print(resp)
+    if resp:
+        dados_processados.append(resp) # adiciona os dados processados à lista
+
+    df_resp = pd.DataFrame(dados_processados) # transforma a lista em df
+    print (df_resp.head())
