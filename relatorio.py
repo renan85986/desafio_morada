ef426974ad42433fd39aba388041e6719bb277a6
renanzao <renannevessilva@gmail.com>
2025-03-16 21:56:21 -0300

    Relatório completo

--- relatorio.py
+++ relatorio.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import sqlite3
+
+largura = 0.30
 
 def carregar_dados():
     conn = sqlite3.connect('D:/Pessoal/desafio_morada/bd/desafio_local.db')  
@@ -19,35 +22,100 @@ lead_selecionado = st.selectbox(" Selecione um Lead:", df_leads["nome_lead"])
 
 df_leads = df_leads.set_index("id")
 lead_info = df_leads.loc[df_leads["nome_lead"] == lead_selecionado, ["nome_lead", "email", "telefone", "orcamento", "localizacao", "estado", "tipo_imovel", "preferencias"]]
-st.write("### Informações do Lead", lead_info)
+lead_info_renomeado = lead_info.rename(columns={
+    "nome_lead": "Nome do Lead",
+    "email": "Email",
+    "telefone": "Telefone",
+    "orcamento": "Orçamento",
+    "localizacao": "Localização",
+    "estado": "Estado",
+    "tipo_imovel": "Tipo de Imóvel",
+    "preferencias": "Preferências"
+})
+st.write("### Informações do Lead", lead_info_renomeado)
 
 df_sugestoes = df_sugestoes.set_index("id")
 sugestao = df_sugestoes[df_sugestoes["nome_lead"] == lead_selecionado]
-st.write("### Empreendimento Sugerido", sugestao)
+sugestao_renomeado = sugestao.rename(columns={
+    "nome_lead": "Nome do Lead",
+    "nome": "Nome do Empreendimento",
+    "justificativa": "Justificativa",
+})
+st.write("### Empreendimento Sugerido", sugestao_renomeado)
 
 if not sugestao.empty:
     empreendimento_info = df_empreendimentos.loc[df_empreendimentos["nome"] == sugestao.iloc[0]["nome"],["nome","localizacao","valor","quartos","banheiros","area","vagas","caracteristicas"]]
-    st.write("### Detalhes do Empreendimento", empreendimento_info)
+    empreendimento_info_renomeado = empreendimento_info.rename(columns={
+        "nome": "Nome do Empreendimento",
+        "localizacao": "Localização",
+        "valor": "Valor",
+        "quartos": "Quartos",
+        "banheiros": "Banheiros",
+        "area": "Área (m²)",
+        "vagas": "Vagas",
+        "caracteristicas": "Características"
+    })
+    st.write("### Detalhes do Empreendimento", empreendimento_info_renomeado)
 
-nome_leads = df_leads["nome_lead"].to_list()
-#print(nome_leads)
+#Gráfico interativo
+st.write("### Orçamento do lead x Valor do empreendimento sugerido")
+lead_orcamento = df_leads.loc[df_leads["nome_lead"] == lead_selecionado, "orcamento"].values[0]
+nome_sugestao_atual = df_sugestoes.loc[df_sugestoes["nome_lead"] == lead_selecionado, "nome"].values[0]
+sugerido_orcamento = df_empreendimentos.loc[df_empreendimentos["nome"] == nome_sugestao_atual, "valor"].values[0]
 
-orcamento_leads = df_leads["orcamento"].to_list()
-#print(orcamento_leads)
+x = ['Orçamento do lead', 'Valor do imóvel']
+valores = [lead_orcamento, sugerido_orcamento]
 
-empreendimentos_sugeridos = (df_sugestoes.loc[df_sugestoes["nome_lead"] == nome_leads, "nome"]).to_list()
-orcamento_sugeridos = df_empreendimentos.loc[df_empreendimentos["nome"].isin(empreendimentos_sugeridos), "valor"].to_list() #isin verifica se os elementos da serie empreendimentos_sugeridos estão na coluna "nome"
-#print(empreendimentos_sugeridos)
-#print(orcamento_sugeridos)
+fig2, ax2 = plt.subplots(figsize=(10,5))
+
+ax2.bar(x,valores, color=['royalblue','red'], width=largura)
+st.pyplot(fig2)
+
 
 st.markdown("---")
-#estado_selecionado = st.selectbox(" Selecione um estado: ", df_leads["nome_lead"])
+
+#Gráficos Gerais
+st.write("### Insights Gerais")
+nome_leads = df_leads["nome_lead"].to_list()
+
+orcamento_leads = df_leads["orcamento"].to_list()
+
+empreendimentos_sugeridos = []
+orcamento_sugeridos = []
+
+for nome_lead in nome_leads:
+    empreendimentos = df_sugestoes.loc[df_sugestoes["nome_lead"] == nome_lead, "nome"].to_list()
+    if empreendimentos: 
+        empreendimento_sugerido = empreendimentos[0]  
+        orcamento_empreendimento = df_empreendimentos.loc[df_empreendimentos["nome"] == empreendimento_sugerido, "valor"].to_list()
+        if orcamento_empreendimento:
+            empreendimentos_sugeridos.append(empreendimento_sugerido)
+            orcamento_sugeridos.append(orcamento_empreendimento[0])  
+        else:
+            orcamento_sugeridos.append(0)  
+    else:
+        orcamento_sugeridos.append(0)  
+
+fig, ax = plt.subplots(figsize=(19, 8))
+
+r1 = np.arange(len(orcamento_leads))
+r2 = [x + largura for x in r1]
+
+ax.bar(r1, orcamento_leads, color='red', width=largura, label='Orçamento Lead')
+ax.bar(r2, orcamento_sugeridos, color='blue', width=largura, label='Valor Empreendimento Sugerido')
+
+ax.set_xlabel('Leads')
+ax.set_ylabel('Orçamento')
+ax.set_xticks([r + largura for r in range(len(orcamento_leads))])
+ax.set_xticklabels(nome_leads)
+
+ax.legend()
+
+st.pyplot(fig)
+
 lead_por_estado = df_leads["estado"].value_counts()
-
 empreendimentos_por_estado = df_empreendimentos["estado"].value_counts()
-
 df_leads_filtrado = df_leads[~df_leads["tipo_imovel"].str.contains(",", na=False)]
-
 cont_tipo_imovel = df_leads_filtrado["tipo_imovel"].value_counts()
 
 df_comp = pd.DataFrame({"Leads": df_leads["estado"].value_counts(), 
